e347575d0bded1cfb8800224194014d6e7f5e2cf
MiftahulSimanto <miftahul.islam.cse@ulab.edu.bd>
2026-05-05 00:20:39 +0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com", // add all external domains you use
+    ],
+  },
+};
+
+export default nextConfig;
